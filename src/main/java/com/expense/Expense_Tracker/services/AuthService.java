c67b56fdaef6ffee8e28308b3fd25e352cb06b5f
Teeksha <teekshasharma364@gmail.com>
2026-02-09 22:59:06 +0530

    Update AuthService.java

--- src/main/java/com/expense/Expense_Tracker/services/AuthService.java
+++ src/main/java/com/expense/Expense_Tracker/services/AuthService.java
@@ -1,17 +1,17 @@
 package com.expense.Expense_Tracker.services;
-
 import com.expense.Expense_Tracker.DAO.UserDAO;
-
 public class AuthService {
 
-    private UserDAO userDAO = new UserDAO();
-
+    private UserDAO userDAO = new UserDAO(); // DAO object to access user data
     public String login(int userId) {
 
+        // Check if user exists in database
         if (userDAO.userExists(userId)) {
+            // Return username if user is found
             return userDAO.getUserNameById(userId);
         }
 
+        // Return null if user does not exist
         return null;
     }
 }
